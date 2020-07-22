a4f2ed205b523170dfcdf5572f3a7bb709ea806d
Dennis <Dennis.portillo@yale.edu>
2020-07-22 09:06:18 -0700

    Dennis

--- src/index.js
+++ src/index.js
@@ -1,1 +1,98 @@
 // write your code here
+document.addEventListener("DOMContentLoaded", ()=> {
+    showImage()
+    let charCard = qs('div.image-card')
+    let charCont = qs(".image-container")
+    // charCard.innerHTML = ""
+})
+
+
+function ce(element) {
+    return document.createElement(element)
+}
+
+function qs(element) {
+    return document.querySelector(element)
+}
+
+function showImage() {
+    fetch("http://localhost:3000/images/1")
+    .then(response => response.json())
+    .then(imag => showoneImage(imag))
+}
+
+
+function showoneImage(imag) {
+    let div = qs('div.image-card')
+    div.className = "image-card"
+
+    let h2 = qs('h2.title')
+    h2.innerText = imag.title
+    h2.className = "title"
+
+    let img = qs("img.image")
+    img.src = imag.image
+    img.className = "image"
+
+    let likesSection = qs('div.likes-section')
+    likesSection.className = "likes-section"
+
+    let imglikes = qs("span.likes")
+    imglikes.className = "likes"
+    imglikes.innerText = imag.likes + " likes"
+
+    let btn = qs("button.like-button")
+    btn.className = "like-button"
+    btn.innerText = "♥"
+
+    let ul = qs('ul.comments')
+    ul.className = "comments"
+
+    let li = qs('ul > li')
+    li.innerText = ""
+
+    let charCard = qs('div.image-card')
+
+    // likesSection.append(imglikes, btn)
+    // ul.appendChild(li)
+    // div.append(h2, img, likesSection, imglikes, btn, ul)
+    // charCard.append(div)
+
+    btn.addEventListener("click", () => {
+        fetch("http://localhost:3000/images/1", {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                likes: imag.likes + 1
+            })
+        })
+        .then(res => res.json())
+        .then(updatedImag => {
+            imglikes.innerText = updatedImag.likes + " likes"
+            imag = updatedImag
+        })
+    })
+
+    const form = qs('form.comment-form')
+    form.addEventListener("submit", ()=> {
+    event.preventDefault()
+    let comment = event.target[0].value
+    // debugger
+    fetch("http://localhost:3000/images", {
+        method: 'POST',
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify({comment: comment})
+    })
+.then(response => response.json())
+.then(comment => {
+    li.innerText = comment
+})
+form.reset()
+})
+}
+
+
